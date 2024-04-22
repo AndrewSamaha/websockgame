34cbdb91f6efa9ec741cc9aab27edf035ddcb203
Andrew Samaha <andrew.samaha@gmail.com>
2024-04-22 19:13:29 -0400

    refactor: Moved game logic to GamePage

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -6,70 +6,56 @@ import {
   Link
 } from "react-router-dom";
 import { enableReactUse } from '@legendapp/state/config/enableReactUse';
-import { Layer } from './components/Layer/Layer'
-import { GAME_SIZE } from './constants/game'
-import './App.css'
-import { VIEWPORT_KEYS } from './constants/input';
-import { useAnimationFrame } from '@haensl/react-hooks';
-import { globalStore } from './state/globalStore';
-import { SocketContext } from './components/SocketProvider/SocketProvider'; // Import the SocketContext
 import { LandingPage } from './components/LandingPage/LandingPage';
 import { GamePage } from './components/GamePage/GamePage';
+import './App.css'
 
-const layer = {
-  zIndex: 0,
-  clickable: false
-}
-
-const mapParams = GAME_SIZE;
-const MIN_VIEWPORT_SPEED = 0.1;
-const VIEWPORT_FRICTION = 0.997;
 
 enableReactUse();
 
-function OgApp() {
-  const { socket, requestCreateUnit } = useContext(SocketContext);
+// function OgApp() {
+//   const { socket, requestCreateUnit } = useContext(SocketContext);
 
-  useEffect(() => {
-    const handleKeyDown = (e) => VIEWPORT_KEYS.includes(e.code) && !e.repeat ? globalStore.viewport.input[e.code].set(Date.now) : 0;
-    const handleKeyUp = (e) => VIEWPORT_KEYS.includes(e.code) && !e.repeat ? globalStore.viewport.input[e.code].set(0) : 0;
-    document.addEventListener('keydown', handleKeyDown);
-    document.addEventListener('keyup', handleKeyUp);
-  }, []);
+//   useEffect(() => {
+//     const handleKeyDown = (e) => VIEWPORT_KEYS.includes(e.code) && !e.repeat ? globalStore.viewport.input[e.code].set(Date.now) : 0;
+//     const handleKeyUp = (e) => VIEWPORT_KEYS.includes(e.code) && !e.repeat ? globalStore.viewport.input[e.code].set(0) : 0;
+//     document.addEventListener('keydown', handleKeyDown);
+//     document.addEventListener('keyup', handleKeyUp);
+//   }, []);
 
-  useAnimationFrame((delta) => {
-    (() => {
-      // apply friction to speed
-      globalStore.viewport.pos.speed.set((speed) => Math.abs(speed) < MIN_VIEWPORT_SPEED ? 0 : speed*VIEWPORT_FRICTION);
+//   useAnimationFrame((delta) => {
+//     (() => {
+//       // apply friction to speed
+//       globalStore.viewport.pos.speed.set((speed) => Math.abs(speed) < MIN_VIEWPORT_SPEED ? 0 : speed*VIEWPORT_FRICTION);
 
-      globalStore.viewport.convertKeysToForce(Date.now());
-      if (globalStore.viewport.force.peek().lengthSq() < .01) {
-        //console.log('force too small', globalStore.viewport.force.peek(), globalStore.viewport.pos.x.peek())
-        return;
-      }
-      // convert forces to translation
-      globalStore.viewport.moveViewport(delta);
-    })();
+//       globalStore.viewport.convertKeysToForce(Date.now());
+//       if (globalStore.viewport.force.peek().lengthSq() < .01) {
+//         //console.log('force too small', globalStore.viewport.force.peek(), globalStore.viewport.pos.x.peek())
+//         return;
+//       }
+//       // convert forces to translation
+//       globalStore.viewport.moveViewport(delta);
+//     })();
 
-    Object.entries(globalStore.interactive.dict.peek()).map(([id, char]) => {
-      const { animate } = char;
-      if (animate) animate(delta, globalStore.viewport, globalStore, 'interactive', mapParams, id, requestCreateUnit)
-    })
+//     Object.entries(globalStore.interactive.dict.peek()).map(([id, char]) => {
+//       const { animate } = char;
+//       if (animate) animate(delta, globalStore.viewport, globalStore, 'interactive', mapParams, id, requestCreateUnit)
+//     })
 
-    Object.entries(globalStore.independent.dict.peek()).map(([id, char]) => {
-      const { animate } = char;
-      if (animate) animate(delta, globalStore.viewport, globalStore, 'independent', mapParams, id)
-    })
+//     Object.entries(globalStore.independent.dict.peek()).map(([id, char]) => {
+//       const { animate } = char;
+//       if (animate) animate(delta, globalStore.viewport, globalStore, 'independent', mapParams, id)
+//     })
 
-  })
+//   })
 
-  return (
+//   return (
     
-    <>
-        <Layer zIndex={layer.zIndex} clickable={layer.clickable} mapParams={mapParams}/>
-    </>
-  )
-}
+//     <>
+//         <Layer zIndex={layer.zIndex} clickable={layer.clickable} mapParams={mapParams}/>
+//     </>
+//   )
+// }
 
 function App() {
   return (
@@ -78,8 +64,9 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Routes>
-          <Route path="/game" element={<GamePage/>} />
-          <Route path="/" element={<LandingPage/>} />
+          <Route exact path="/" element={<LandingPage/>} />
+          <Route exact path="/game" element={<GamePage/>} />
+          <Route path="*" element={<LandingPage/>} />
         </Routes>
       </div>
     </Router>
--- ui/src/components/GamePage/GamePage.jsx
+++ ui/src/components/GamePage/GamePage.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { enableReactUse } from "@legendapp/state/config/enableReactUse";
+import { Layer } from "../Layer/Layer";
+import { GAME_SIZE } from "../../constants/game";
+import { VIEWPORT_KEYS } from "../../constants/input";
+import { useAnimationFrame } from "@haensl/react-hooks";
+import { globalStore } from "../../state/globalStore";
+import { SocketContext } from "../SocketProvider/SocketProvider"; // Import the SocketContext
+
+import "./GamePage.css";
+
+const layer = {
+    zIndex: 0,
+    clickable: false
+};
+
+const mapParams = GAME_SIZE;
+const MIN_VIEWPORT_SPEED = 0.1;
+const VIEWPORT_FRICTION = 0.997;
 
 export const GamePage = () => {
+    const { socket, requestCreateUnit } = useContext(SocketContext);
+
+    useEffect(() => {
+        const handleKeyDown = (e) => VIEWPORT_KEYS.includes(e.code) && !e.repeat ? globalStore.viewport.input[e.code].set(Date.now) : 0;
+        const handleKeyUp = (e) => VIEWPORT_KEYS.includes(e.code) && !e.repeat ? globalStore.viewport.input[e.code].set(0) : 0;
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+    }, []);
+    
+    useAnimationFrame((delta) => {
+        (() => {
+          // apply friction to speed
+          globalStore.viewport.pos.speed.set((speed) => Math.abs(speed) < MIN_VIEWPORT_SPEED ? 0 : speed*VIEWPORT_FRICTION);
+    
+          globalStore.viewport.convertKeysToForce(Date.now());
+          if (globalStore.viewport.force.peek().lengthSq() < .01) {
+            //console.log('force too small', globalStore.viewport.force.peek(), globalStore.viewport.pos.x.peek())
+            return;
+          }
+          // convert forces to translation
+          globalStore.viewport.moveViewport(delta);
+        })();
+    
+        Object.entries(globalStore.interactive.dict.peek()).map(([id, char]) => {
+          const { animate } = char;
+          if (animate) animate(delta, globalStore.viewport, globalStore, 'interactive', mapParams, id, requestCreateUnit)
+        })
+    
+        Object.entries(globalStore.independent.dict.peek()).map(([id, char]) => {
+          const { animate } = char;
+          if (animate) animate(delta, globalStore.viewport, globalStore, 'independent', mapParams, id)
+        })
+    
+      })
+
     return (
-        <div>
-        <h1>Game Page</h1>
-        </div>
+        <>
+            <Layer zIndex={layer.zIndex} clickable={layer.clickable} mapParams={mapParams}/>
+        </>
     );
 };
